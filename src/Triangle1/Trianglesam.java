62f591d8baba6e3c4ad0e6cda896714e7e4e6f68
drsaurabh73 <drsaurabh73@gmail.com>
2022-03-08 21:59:08 +0000

    Triangle

---
+++ src/Triangle1/Trianglesam.java
@@ -0,0 +1,12 @@
+package Triangle1;
+
+public class Trianglesam {
+    public static void main (String [] args){
+        System.out.println(" * * * * * * * * * ");
+        System.out.println("  * * * * * * *  ");
+        System.out.println("   * * * * *    ");
+        System.out.println("     * * *      ");
+        System.out.println("       *  ");
+
+    }
+}
